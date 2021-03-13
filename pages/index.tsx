283b75d9c087f15a42e94104d3902e4bb52281c8
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-13 09:12:36 -0500

    Change dark text color to white

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="absolute w-32 h-32 rounded-full top-1/4 left-1/3 bg-white dark:bg-yellow-200" />
         <div className="container flex flex-row-reverse">
           <div className="absolute w-1/2 top-1/4 pr-8 lg:pr-24">
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl text-red-600">A storybook marriage between web development and illustrative design.</h1>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl text-red-600 dark:text-white">A storybook marriage between web development and illustrative design.</h1>
           </div>
         </div>
         <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen" />
