b06ce9587a8c5a3a710abd9e13768dfcdfcaa786
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-12 20:17:02 -0500

    Set logo as a button

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
       <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen">
         <div className="flex flex-none justify-between top-0 z-40 w-full max-w-8xl mx-auto sticky h-20">
           <div className="flex flex-none pl-4 items-center">
-            <a className="overflow-hidden w-auto" onClick={setCurrentTheme}>
+            <button type="button" className="overflow-hidden w-auto" onClick={setCurrentTheme}>
               <span className="sr-only">King Post Studio home page</span>
               <Logo />
-            </a>
+            </button>
             <h1 className="px-4">King Post Studio</h1>
           </div>
           <div className="flex flex-none pr-4 items-center">
