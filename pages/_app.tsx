83ad0bf46e1ed62358f0ed28213b91466343afe1
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-12 20:08:57 -0500

    Setup dark theme toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
+import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Head>
         <title>King Post Studio</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
+import { useTheme } from 'next-themes';
 import Logo from '../components/Logo';
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
+  const setCurrentTheme = (): void => setTheme(theme === 'dark' ? 'light' : 'dark');
+
   return (
     <div className="bg-blue-200 dark:bg-gray-900">
       <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen">
-        <div className="flex flex-none top-0 z-40 w-full max-w-8xl mx-auto sticky h-20">
+        <div className="flex flex-none justify-between top-0 z-40 w-full max-w-8xl mx-auto sticky h-20">
           <div className="flex flex-none pl-4 items-center">
-            <a className="overflow-hidden w-auto" href="/">
+            <a className="overflow-hidden w-auto" onClick={setCurrentTheme}>
               <span className="sr-only">King Post Studio home page</span>
               <Logo />
             </a>
-            <h1 className="color-gray-800 dark:color-white px-4">King Post Studio</h1>
+            <h1 className="px-4">King Post Studio</h1>
+          </div>
+          <div className="flex flex-none pr-4 items-center">
+
           </div>
         </div>
       </div>
