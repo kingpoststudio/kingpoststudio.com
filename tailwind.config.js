1b4d4825ae2c5558be33833dc9156121d520ecb0
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-12 20:56:17 -0500

    Dark mode fix for background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: () => ({
-        'yosemite-day': 'url(/images/yosemite-day.png)',
-        'yosemite-night': 'url(/images/yosemite-night.png)',
+        yosemiteDay: 'url(/images/yosemite-day.png)',
+        yosemiteNight: 'url(/images/yosemite-night.png)',
       }),
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ['dark'],
+    },
   },
   plugins: [],
 };
