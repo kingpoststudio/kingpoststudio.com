ac948560513713b38e5086c77ebad23994c41152
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-12 23:34:44 -0500

    Set absolutes relative to container

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div className="bg-white dark:bg-gray-800">
       <Header />
       <div className="container mx-auto bg-yellow-200 dark:bg-dark-gray transition duration-700 ease-in-out">
+        <div className="absolute w-32 h-32 rounded-full top-1/4 left-1/3 bg-white dark:bg-yellow-200" />
+        <div className="absolute top-1/4 left-1/3">
+          <h1 className="dark:text-red-600">Once upon a time...</h1>
+        </div>
         <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen" />
-        <div className="absolute w-32 h-32 rounded-full top-1/4 left-1/2 bg-white dark:bg-yellow-200" />
-      </div>
-      <div className="absolute w-full flex justify-center top-1/4">
-        <h1 className="dark:text-red-600">Once upon a time...</h1>
       </div>
     </div>
   );
