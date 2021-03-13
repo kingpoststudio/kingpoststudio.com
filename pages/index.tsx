2d582469cdaec7908de04de1ace1312b19170d1c
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-12 22:43:51 -0500

    Add sun/moon transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="bg-white dark:bg-gray-800">
       <Header />
       <div className="container mx-auto bg-yellow-200 dark:bg-gray-800 transition duration-700 ease-in-out">
-        <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen" />
+        <div className="absolute w-32 h-32 rounded-full top-1/4 left-1/2 bg-white dark:bg-yellow-300" />
+        <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen">
+        </div>
       </div>
     </div>
   );
