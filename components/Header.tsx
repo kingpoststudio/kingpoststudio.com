3f2feb6f2194442ac8867b7ef253dbb4ad05ed9d
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-14 18:42:27 -0400

    Add github logo, not using yet

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,7 @@
-import { useTheme } from 'next-themes';
 import Logo from './Logo';
 import DarkModeToggle from './DarkModeToggle';
 
 export default function Header() {
-  const { theme, setTheme } = useTheme();
-
-  const setCurrentTheme = (): void => setTheme(theme === 'dark' ? 'light' : 'dark');
-
   return (
     <header className="flex flex-none justify-between bg-white dark:bg-black top-0 z-50 px-4 sm:px-12 w-full max-w-8xl mx-auto sticky h-20">
       <div className="flex flex-none items-center text-red-600">
