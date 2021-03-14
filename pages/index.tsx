acf134880d5043160c696571ff91730e69c2bc3b
tlhfckoctbcr <stu@campgrantnc.com>
2021-03-13 23:10:40 -0500

    Update text, for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <div className="bg-white dark:bg-gray-800">
       <Header />
-      <div className="container mx-auto bg-yellow-200 dark:bg-dark-gray transition duration-700 ease-in-out">
-        <div className="absolute w-32 h-32 rounded-full top-1/4 left-1/3 bg-red-100 dark:bg-blue-200" />
-        <div className="container flex flex-row-reverse">
-          <div className="absolute w-1/2 top-1/4 pr-8 lg:pr-24">
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl text-red-600 dark:text-white">A storybook marriage between web development and illustrative design.</h1>
+      <div className="lg:container mx-auto bg-yellow-200 dark:bg-dark-gray transition duration-700 ease-in-out">
+        <div className="container -mt-20">
+          <div className="absolute w-1/2 top-24 ml-4 bg-white dark:bg-transparent border border-red-600 dark:border-transparent text-center">
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl text-red-600 dark:text-gray-200 p-4 shadow-md">
+              Where web development & illustrative design meet.
+            </h1>
           </div>
         </div>
         <div className="bg-yosemite-day dark:bg-yosemite-night bg-cover bg-center h-screen" />
